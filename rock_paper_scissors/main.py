3e197d0f79e0756d9cc4ed4b82a8728340ac8f4a
QJ75 <56568420+QJ75@users.noreply.github.com>
2022-02-10 14:56:33 +0300

    Update main.py

--- rock_paper_scissors/main.py
+++ rock_paper_scissors/main.py
@@ -1,18 +1,20 @@
 import random
+import math
 
 def play():
     user = input('что ты выберешь? (к) для камня, (н) для ножниц, (б) для бумаги\n')
     user = user.lower()
 
     computer = random.choice(['к','н','б'])
+
     if user == computer:
-        return 'Вы и компютер выбрали одно и тоже {}. Это ничья'.format(computer)
+        return (0, user, computer)
     
     # к > н, б > к, н > б
     if combat_win(user, computer):
-        return 'Вы выбрали {} и компютер выбрал {}. Ты выиграл'.format(user, computer)
-
-    return 'Вы выбрали {} и компютер выбрал {}. Ты проиграл'.format(user, computer)
+        return (1, user, computer)
+        
+    return (-1, user, computer)
 
 def combat_win(player, oponent):
     if  (player == 'к' and oponent == 'н' ) or (player == 'б' and oponent == 'к' ) or (player == 'н' and oponent == 'б' ):
@@ -20,5 +22,29 @@ def combat_win(player, oponent):
         
     return False
 
+def best_player(n):
+    #игра против компютера пока не достигнута победа в n раз
+    player_wins = 0
+    computer_wins = 0
+    wins_necesery = math.ceil(n/2)
+    print(wins_necesery)
+    while player_wins < wins_necesery and computer_wins < wins_necesery:
+        
+        result, user, computer = play()
+        
+        if result == 0:
+            print('Вы и компютер выбрали одно и тоже {}. Это ничья\n'.format(user))
+        elif result == 1:
+            player_wins +=1
+            print('Вы выбрали {} и компютер выбрал {}. Ты выиграл\n'.format(user, computer))
+        else:
+            computer_wins += 1
+            print('Вы выбрали {} и компютер выбрал {}. Ты проиграл\n'.format(user, computer))
+        print('\n')
+    if player_wins > computer_wins:
+        print('Ты одержал победу за {} раз. Молодец!!!\n'.format(n))
+    else:
+        print('Ты проиграл компьютеру за {} раз. Попробуй еще раз!\n'.format(n))
+
 if __name__ == '__main__':
-    print(play())
+    best_player(3) #2
